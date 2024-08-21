f58608f03194ae0161cf8ea8fb3a5e4c26d295a7
Modin Sergei <jetleebruce@gmail.com>
2024-08-21 16:54:37 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
       <header className="w-max">
         <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
       </header>
       <Button>Button</Button>
     </main>
